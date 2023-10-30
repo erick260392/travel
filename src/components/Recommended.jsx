f9c5af8358ee2c98fe9f8d93f8464de49230df3b
erick260392 <erickliconamorales2016@gmail.com>
2023-10-30 16:36:44 -0600

    Recomendaciones

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import React,{useState} from 'react';
+import styled from 'styled-components';
+import Destination1 from '../assets/Destination1.png';
+import Destination2 from '../assets/Destination2.png';
+import Destination3 from '../assets/Destination3.png';
+import Destination4 from '../assets/Destination4.png';
+import Destination5 from '../assets/Destination5.png';
+import Destination6 from '../assets/Destination6.png';
+import info1 from '../assets/info1.png';
+import info2 from '../assets/info2.png';
+import info3 from '../assets/info3.png';
+
+
+ 
 
 export default function Recommended() {
+
+  const data =[
+    {
+      image:Destination1,
+      title:'Singapure',
+      subtitle:'Singapure es la capital de la republica de Singapure',
+      cost:'38,000',
+      duration: 'Dos noches y dos Dias',
+    },
+    {
+      image:Destination2,
+      title:'Thailand',
+      subtitle:'Thailand es un Lugar del Sureste Asiatico, Este lugar es para ti lo sabes',
+      cost:'54,200',
+      duration:'Dos, noches y dos Dias',
+    },
+    {
+      image:Destination3,
+      title:'Paris',
+      subtitle:'Paris, es la capital de francia y la mayor de Europa',
+      cost:'45,000',
+      duration:'Dos noches',
+    },
+    {
+      image:Destination4,
+      title:'New Zealand',
+      subtitle:'Nueva zelanda es una instala dentro de un Pais',
+      cost:'45,000',
+      duration:'Un dia y una Noche',
+    },
+    {
+      image:Destination5,
+      title:'Bora Bora',
+      subtitle:'Bora Bora es una pequeña isla en el sureste del pacifico de Northwest',
+      cost:'95,000',
+      duration:'Dos dias y Dos noches',
+    },
+    {
+      image:Destination6,
+      title:'London',
+      subtitle:'London es la capital de Inglaterra',
+      cost:'38,000',
+      duration:'Tres dias y Dos noches',
+    }
+  ]
+
+  const packages =[
+    'El escape de Fin de Semana',
+    'El paquete Navideño',
+    'El tour grupal',
+    'Viaje para que te reecuentres Contigo mismo',
+  ]
+
   return (
     <div></div>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,23 +10,23 @@ export default function Services() {
   const data = [
     {
       icon: service1,
-      title: 'obten los mejores Precios',
-      subtitle: 'paga con nuestra la aplicacion y obten grandes recompenzas'
+      title: 'Obten los mejores Precios',
+      subtitle: 'Paga con nuestra la aplicacion y obten grandes recompenzas'
     },
     {
       icon: service2,
-      title: "libre de Covid",
-      subtitle: 'todos los hoteles son sanitizados libres de covid'
+      title: "Libre de Covid",
+      subtitle: 'Todos los hoteles son sanitizados libres de covid'
     },
     {
       icon: service3,
-      title: 'flexibilidad de Pago',
+      title: 'Flexibilidad de Pago',
       subtitle: 'Divirtete y obten flexibilidad en tus pagos aparta tu viaje',
     },
     {
       icon: service4,
-      title: 'Encuentra el mejor Lugar para Viajar Cerca de ti',
-      subtitle: 'encuentra los mejores Lugares para viajar y Hoteles para Viajar cerca de ti en un solo click',
+      title: 'Encuentra el mejor Lugar para Viajar cerca de ti',
+      subtitle: 'Encuentra los mejores Lugares para viajar y Hoteles para Viajar cerca de ti en un solo click',
 
     }
   ]
@@ -37,11 +37,13 @@ export default function Services() {
   return (
     <Section id='services'>
       {data.map((service, index) => {
-        return(
-          <div className="service">
+        return (
+          <div key={index} className="service">
             <div className="icon">
-              
+              <img src={service.icon} alt="" />
             </div>
+            <h3>{service.title}</h3>
+            <p>{service.subtitle}</p>
           </div>
         );
 
@@ -53,6 +55,33 @@ export default function Services() {
 
 
 const Section = styled.section`
-
+padding:5rem 0;
+display:grid;
+grid-template-columns:repeat(4,1fr);
+gap:1rem;
+.service{
+  display:flex;
+  flex-direction:column;
+  gap:1rem;
+  padding:2rem;
+  background-color: aliceblue;
+  box-shadow:rgba(100,100,111,0.2) 0px 7px 29px 0px;
+  transition: 0.3s ease-in-out;
+  &:hover{
+    transform:translateX(0.4rem)translateY(-1rem);
+    box-shadow:rgba(0,0,0,0.35) 0px 5px 15px;
+  }
+  .icon{
+    img{
+      height:2.4rem;
+    }
+  }
+}
+@media screen and (min-width: 280px) and (max-width: 720px) {
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+}
+@media screen and (min-width: 720px) and (max-width: 1080px) {
+  grid-template-columns: repeat(2, 1fr);
+}
 
 `;
